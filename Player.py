08d6fdaf96c48676d944ad4452518ab046739bc1
Laurens de Bruin <laurens.de.bruin99@gmail.com>
2025-08-01 20:49:35 +0200

    Add player that can guess cities

---
+++ Player.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+import time
+
+class Player:
+    def __init__(self):
+        options = Options()
+        options.add_argument("--start-maximized")
+        self.driver = webdriver.Chrome(options=options)
+        self.driver.maximize_window()
+        self.driver.get("https://cityquiz.io/quizzes/world")
+
+        wait = WebDriverWait(self.driver, 10)
+        self.input_box = wait.until(
+            EC.element_to_be_clickable((By.ID, "city-input"))
+        )
+
+    def guess(self, city):
+        time.sleep(0.5)
+        self.input_box.send_keys(city)
+        time.sleep(0.5)
+        self.input_box.send_keys(Keys.RETURN)
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import DataReader as dataReader
+import Player as player
 
 if __name__ == "__main__":
     reader = dataReader.DataReader()
     reader.prepare_data()
 
-    print(reader.get_data())
+    data = reader.get_data()
+
+    player = player.Player()
+
+    player.guess("Amsterdam")
+    player.guess("Bejing")
+
+    input("Press enter to exit...")
